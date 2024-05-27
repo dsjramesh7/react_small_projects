a1a6567e8a4ddf756ecb03bd5be767ee69ac35e4
dsjramesh7 <rameshdussa07@gmail.com>
2024-05-28 01:38:23 +0530

    practice

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -4,40 +4,80 @@ import "./scroll.css";
 export default function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
-  async function FetchApIData(get_url) {
+  async function fetchData(getUrl) {
     try {
       setLoading(true);
-      const response = await fetch(get_url);
-      const gotData = await response.json();
-      if (gotData && gotData.products && gotData.products.length > 0) {
-        setData(gotData.products);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
         setLoading(false);
       }
-      setLoading(false);
     } catch (e) {
       console.log(e);
-      setErrorMsg(e.message);
+      setErrorMessage(e.message);
     }
   }
 
   useEffect(() => {
-    FetchApIData(url);
+    fetchData(url);
   }, [url]);
 
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  // console.log(data, scrollPercentage);
+
+  if (errorMessage) {
+    return <div>Error ! {errorMessage}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading data ! Pleaae wait</div>;
+  }
+
   return (
-    <>
-      <div>
+    <div>
+      <div className="top-container">
         <h1>Custom Scroll Indicator</h1>
-        <div className="data-container">
-          {data && data.length > 0 ? (
-            data.map((item, i) => <p key={i}>{item.title}</p>)
-          ) : (
-            <p>data not fetched</p>
-          )}
+        <div className="scroll-progress-tracking-container">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
         </div>
       </div>
-    </>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem, i) => <p key={i}>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
   );
 }
