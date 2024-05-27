71b1948775f517b72314b77f716a8eca2b57d787
dsjramesh7 <rameshdussa07@gmail.com>
2024-05-27 20:15:40 +0530

    practice

--- src/components/light-dark-mode/Ldmode.jsx
+++ src/components/light-dark-mode/Ldmode.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import useLocalStorage from "./useLocalStorage";
+import "./theme.css";
 
 const Ldmode = () => {
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+  const handleToggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  console.log(theme);
   return (
-    <div className="light-dark-mode">
+    <div className="light-dark-mode" data-theme={theme}>
       <div className="container">
         <p>Hello World !</p>
-        <button>Change Theme</button>
+        <button onClick={handleToggleTheme}>Change Theme</button>
       </div>
     </div>
   );
