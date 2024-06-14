514226d98a6306bf56d122f79fdbc205923ac5cb
dsjramesh7 <rameshdussa07@gmail.com>
2024-06-15 00:10:58 +0530

    practice

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -2,52 +2,32 @@ import React, { useState } from "react";
 import Tasks from "./taskComponents/Tasks";
 
 const TodoList = () => {
-  const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
-  const [editValue, setEditValue] = useState(-1); 
-  const [edit, setEdit] = useState(false); 
+  const [tasks, setTasks] = useState([]);
 
-  const handleAddTask = () => {
-    if (edit && input.trim() !== "") {
-      const updatedTasks = [...tasks];
-      updatedTasks[editValue] = input;
-      setTasks(updatedTasks);
-      setInput("");
-      setEdit(false);
-      setEditValue(-1);
-    } else if (!edit && input.trim() !== "") {
+  const handleAddTasks = () => {
+    if (input.trim() !== "") {
       setTasks([...tasks, input]);
       setInput("");
     }
   };
 
-  const handleDeleteTask = (taskId) => {
-    setTasks(tasks.filter((task, index) => index !== taskId));
+  const handleDeleteTask = (currenIndex) => {
+    setTasks(tasks.filter((item, index) => index !== currenIndex));
   };
 
-  const handleEditTask = (taskId) => {
-    setInput(tasks[taskId]); 
-    setEditValue(taskId);
-    setEdit(true);
-  };
-
+  // console.log(tasks);
   return (
     <>
-      <div className="flex gap-x-3">
+      <div className="flex gap-x-4">
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <div onClick={handleAddTask}>{edit ? "Update" : "Add"}</div>
+        <div onClick={handleAddTasks}>Add</div>
       </div>
-      <div>
-        <Tasks
-          tasks={tasks}
-          handleDeleteTask={handleDeleteTask}
-          handleEditTask={handleEditTask}
-        />
-      </div>
+      <Tasks tasks={tasks} handleDeleteTask={handleDeleteTask} />
     </>
   );
 };
--- src/components/todoList/taskComponents/Tasks.jsx
+++ src/components/todoList/taskComponents/Tasks.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 
-const Tasks = ({ tasks, handleDeleteTask, handleEditTask }) => {
+const Tasks = ({ tasks, handleDeleteTask }) => {
   return (
     <>
       {tasks && tasks.length > 0 ? (
-        tasks.map((item, i) => (
-          <div className="flex gap-x-8 gap-y-2" key={i}>
-            <strong>{i + 1}</strong>
-            <p>{item}</p>
+        tasks.map((task, i) => (
+          <div className="flex gap-x-8" key={i}>
+            <p>{task}</p>
             <div onClick={() => handleDeleteTask(i)}>Delete</div>
-            <div onClick={() => handleEditTask(i)}>Edit</div>
+            <div>Edit</div>
           </div>
         ))
       ) : (
-        <p>No Data Present!!!</p>
+        <p>No task to do!!!</p>
       )}
     </>
   );
